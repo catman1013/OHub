99dd9fadea4b06dcfe254fddcc21229b72210a07
catman1013 <h2u3u5848@gmail.com>
2022-02-13 00:08:29 +0900

    fix

--- db/migrate/20211118010639_add_tech_category_to_articles.rb
+++ db/migrate/20211118010639_add_tech_category_to_articles.rb
@@ -1,6 +1,6 @@
 class AddTechCategoryToArticles < ActiveRecord::Migration[5.2]
   def change
-    addcolumn :articles, :category, :string
+    add_column :articles, :category, :string
     add_column :articles, :tech_category, :string
   end
 end
