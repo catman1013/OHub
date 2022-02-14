9637b18c6fe6ad8e835b34dae838e58bdc04c611
catman1013 <h2u3u5848@gmail.com>
2022-02-14 20:55:41 +0900

    mouseover機能消す

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -38,15 +38,3 @@ window.onload = function() {
   });
 
 }
-
-document.addEventListener('tubolinks:load', function() {
-  document.querySelectorAll('td').forEach(function(td){
-    td.addEventListener('mouseover', function(e) {
-      e.currentTarget.style.backgroundColor = '#eff';
-    });
-
-    td.addEventLstener('mouseout', function(e) {
-      e.currentTarget.style.backgroundColor = '';
-    });
-  });
-});
